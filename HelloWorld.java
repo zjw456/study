c632e421a59da0bf1f689327187beaf4d0b465a7
zjw456 <773554401@qq.com>
2019-05-06 10:25:40 +0800

    在master分支下输出一条信息

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world Two");
+		System.out.println("在master分支下输出一条信息");
 	}
 }
